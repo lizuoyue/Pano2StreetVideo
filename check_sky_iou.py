3660d6be5e369ae358f92475866156a4cc76f681
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.lan>
2020-06-07 18:42:25 +0900

    check sky

--- check_sky_iou.py
+++ check_sky_iou.py
@@ -4,8 +4,9 @@ from PIL import Image
 
 if __name__ == '__main__':
 
+	res = []
 	files = sorted(glob.glob('/home/zoli/xiaohu_new_data/train2_new/*_proj_depth.png'))
-	for file in files:
+	for file in tqdm.tqdm(files):
 
 		depth = np.array(Image.open(file).convert('L'))
 		label = np.array(Image.open(file.replace('_proj_depth.png', '_street_label.png')).convert('RGB')).astype(np.int32)
@@ -15,4 +16,7 @@ if __name__ == '__main__':
 
 		sky1 = depth > 250
 		sky2 = (label ** 2).sum(axis=-1) == 0
-		print((sky1 & sky2).sum() / (sky1 | sky2).sum())
+		res.append(((sky1 & sky2).sum() / (sky1 | sky2).sum(), file))
+
+	for item in res:
+		print(item)
