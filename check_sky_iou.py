6e03926360af538a9d05257f332bc13c3f3f468b
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.lan>
2020-06-07 18:40:51 +0900

    check sky

--- check_sky_iou.py
+++ check_sky_iou.py
@@ -4,11 +4,11 @@ from PIL import Image
 
 if __name__ == '__main__':
 
-	files = sorted(glob.glob('/home/zoli/xiaohu_new_data/train2_new/*_sate_depth.png'))
+	files = sorted(glob.glob('/home/zoli/xiaohu_new_data/train2_new/*_proj_depth.png'))
 	for file in files:
 
 		depth = np.array(Image.open(file).convert('L'))
-		label = np.array(Image.open(file.replace('_sate_depth.png', '_street_label.png')).convert('RGB')).astype(np.int32)
+		label = np.array(Image.open(file.replace('_proj_depth.png', '_street_label.png')).convert('RGB')).astype(np.int32)
 		label[..., 0] -= 70
 		label[..., 1] -= 130
 		label[..., 2] -= 180
